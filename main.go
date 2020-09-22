3d421d55ed8531ef57a9097972518bf8238d0484
johansundell <johansundell@gmail.com>
2020-09-22 21:29:53 +0200

    Added concurrency to web calls

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/johansundell/mark-gantt/fmsdata"
@@ -17,13 +18,32 @@ func main() {
 	}
 	defer db.Logout()
 	fmt.Println(time.Now())
-	for i := 0; i < 2000; i++ {
-		resp, err := db.GetAllFrom("sudde")
+	wg := sync.WaitGroup{}
+	ws := make(chan struct{}, 100)
+	for i := 0; i < 25000; i++ {
+		/*resp, err := db.GetAllFrom("sudde")
 		if err != nil {
 			log.Fatal(err)
 		}
-		_ = resp
+		_ = resp*/
 		//fmt.Println(string(resp))
+
+		// Get them all at once
+		wg.Add(1)
+		ws <- struct{}{}
+		go func(n int, w chan struct{}, wg *sync.WaitGroup) {
+			defer wg.Done()
+			resp, err := db.GetAllFrom("sudde")
+			if err != nil {
+				//log.Fatal(err)
+				fmt.Println(err)
+			}
+			//fmt.Println(n)
+			_ = resp
+			<-w
+		}(i, ws, &wg)
+
 	}
+	wg.Wait()
 	fmt.Println(time.Now())
 }
