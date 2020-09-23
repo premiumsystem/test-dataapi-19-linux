717f6eba6c141d2a84f907d09dfbd633bd39734d
johansundell <johansundell@gmail.com>
2020-09-23 20:51:49 +0200

    Added some comments

--- config.go
+++ config.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 )
 
+// The type of setting we support in our json file
 type appSettings struct {
 	Host          string `json:"host"`
 	Filename      string `json:"filename"`
@@ -17,24 +18,32 @@ type appSettings struct {
 	NoOfConcurret int    `json:"no_of_conccurent"`
 }
 
+// The filename of our settings file
 const filenameSettings = "settings.json"
 
+// Global variable to our settings
 var settings appSettings
 
+// All init functions runs before our main func
 func init() {
+	// Get the path and name to where our program is
 	ex, err := os.Executable()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Get the directory from that
 	dir, _ := filepath.Split(ex)
+	// Read our settings file
 	dat, err := ioutil.ReadFile(dir + filenameSettings)
 	if err != nil {
+		// Wow, we did not have a settings.json file; so we create an empty one
 		data, _ := json.Marshal(settings)
 		ioutil.WriteFile(dir+filenameSettings, data, 0664)
 		log.Fatal("settings.json missing, " + err.Error())
 	}
 
+	// Read the settings from our json file into our struct
 	if err := json.Unmarshal(dat, &settings); err != nil {
 		log.Fatal(err)
 	}
