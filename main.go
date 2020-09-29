7a87ad5e84925de6aec477f10dafcca30b441351
johansundell <johansundell@gmail.com>
2020-09-29 21:29:16 +0200

    Cleanup

--- main.go
+++ main.go
@@ -2,83 +2,85 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"sync"
-	"time"
 
 	"github.com/johansundell/fms-data-api/fmsdata"
 )
 
-type responce struct {
-	resp []byte
-	err  error
+var wg, wg2 sync.WaitGroup
+
+type Job struct {
+	Id     int
+	Work   string
+	Result []byte
+	err    error
 }
 
-func mainOLD() {
-	// Set the login info to our database
-	db := fmsdata.NewDataBase(settings.Host, settings.Filename, settings.User, settings.Pass)
-	// Start a new session to the database, abort on fail
-	if err := db.Login(); err != nil {
-		log.Fatal(err)
-	}
-	// Wait until this functions ends before logout
-	defer db.Logout()
-	// Print the current time
-	start := time.Now()
-	fmt.Println(start)
-	// Get a waitgroup so we can handle new routines start up and end to keep track of them
-	wg := sync.WaitGroup{}
-	// Make a channel array of a fixed size so we can keep track of how many we can run at the same time
-	// When all channels in this aray are filled, the loop below will wait until one is set free ;)
-	ws := make(chan responce, settings.NoOfConcurret)
-
-	//errors := make(chan error, 0)
-	//results := make(chan []byte, 0)
-
-	// Loop over and start requests
+func produce(jobs chan<- *Job) {
+	// Generate jobs:
 	for i := 0; i < settings.NoOfRequest; i++ {
-		// Add one to our waitgroup
-		wg.Add(1)
-		// Init our dummy struct
-		ws <- responce{}
-		// Start a new routine running this function
-		go func(n int, w chan responce, wg *sync.WaitGroup) {
-			// Wait until this function are done before removing one from our waitgroup
-			defer wg.Done()
-			// Get the data from FileMaker and print the error on fail
-			resp, err := db.GetAllFrom("sudde")
-			if err != nil {
-				//fmt.Println(err)
-				//errors <- err
-				w <- responce{resp: nil, err: err}
-				return
-			}
-			// We don't really care about the data we got
-			_ = resp
-			//results <- resp
-			// Free the channel so it can be used again
-			w <- responce{resp: resp, err: nil}
-			return
-			//<-w
-		}(i, ws, &wg)
-
+		jobs <- &Job{Id: i, Work: ""}
 	}
+	close(jobs)
+}
 
-	//r := <-ws
-	//fmt.Println(r)
-	/*select r := <-ws{
-	case r.err:
-		fmt.Println(err)
-	case r.resp:
-		fmt.Println(string(r.resp))
-	}*/
+func consume(id int, jobs <-chan *Job, results chan<- *Job) {
+	defer wg.Done()
+	// Do work here
+	for job := range jobs {
+		resp, err := makeCallToFms("sudde")
+		job.Result, job.err = resp, err
+		results <- job
+	}
+}
 
-	// Wait until all routines in our waitgroup are done
-	wg.Wait()
+func makeCallToFms(layout string) ([]byte, error) {
+	// Login to filemaker
+	db := fmsdata.NewDataBase(settings.Host, settings.Filename, settings.User, settings.Pass)
+	if err := db.Login(); err != nil {
+		return nil, err
+	}
+	// Get the data from a layout
+	resp, err := db.GetAllFrom(layout)
+	// Logout from filemaker
+	db.Logout()
+	return resp, err
 
-	// Print the current time
-	end := time.Now()
-	fmt.Println(end)
-	// Print the diff and request we sent
-	fmt.Println(end.Sub(start).Seconds(), "sec and ran", settings.NoOfRequest, "request")
+}
+
+func analyze(results <-chan *Job) {
+	defer wg2.Done()
+	for job := range results {
+		if job.err != nil {
+			fmt.Println("error", job.Id, job.err)
+		} else {
+			if settings.ShowDone {
+				fmt.Println("job", job.Id, "done")
+			}
+		}
+	}
+}
+
+func main() {
+	jobs := make(chan *Job, 3000)    // Buffered channel
+	results := make(chan *Job, 3000) // Buffered channel
+
+	// Start consumers:
+	for i := 0; i < settings.NoOfConcurret; i++ { // Prepare our workers
+		wg.Add(1)
+		go consume(i, jobs, results)
+	}
+	// Start producing
+	go produce(jobs)
+
+	// Start analyzing:
+	wg2.Add(1)
+	go analyze(results)
+
+	wg.Wait() // Wait all consumers to finish processing jobs
+
+	// All jobs are processed, no more values will be sent on results:
+	close(results)
+
+	wg2.Wait() // Wait analyzer to analyze all results
 }
